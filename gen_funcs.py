e2227cd512f495c4e5ed20935c29a3256f050660
Adam Koval <adam_koval@hotmail.com>
2024-10-08 12:29:24 +0100

    Moved more functionality from main_figures_for_pub.py to gen_funcs.py

--- gen_funcs.py
+++ gen_funcs.py
@@ -1,6 +1,31 @@
 # General functions for data loading and processing
 import pandas as pd
 import glob
+import json
+import os
+
+import ranked_funcs as rf
+
+
+def load_paths_all(path_paths):
+    """
+    Loads all path data from .json file
+    In:
+        > path_paths - (str) path to paths.json file
+    Out:
+        > dict_paths - (dct) dictionary containing all paths
+    """
+    # Load paths
+    print("Reading paths", flush=True)
+    with open(path_paths, "r") as f:
+        dict_paths = json.load(f)
+    
+    # Check that paths exist, if not, create them
+    for key in dict_paths:
+        if not os.path.exists(dict_paths[key]):
+            print(f"Creating directory {dict_paths[key]}", flush=True)
+            os.makedirs(dict_paths[key])
+    return dict_paths
 
 
 def load_paths_psliceout(path_data, selected_figs):
@@ -16,6 +41,30 @@ def load_paths_psliceout(path_data, selected_figs):
     for fig in selected_figs:
         paths_psliceout[fig] = [i for i in glob.glob(path_data + fig + "/*")]
     return paths_psliceout
+
+
+def iteratively_load_data(paths_psliceout):
+    """
+    Iteratively loads data from psliceout files which are given by paths_psliceout.
+    In:
+        > paths_psliceout - (dct) paths to psliceout files
+    Out:
+        > data_all - (dct) dictionary containing all data
+    """
+    print("Loading data", flush=True)
+    data_all = {}
+    for key in paths_psliceout:
+        for path in paths_psliceout[key]:
+            print(f"Checking if data has already been read into dict", flush=True)
+            sub_key = path.split("/")[-1].strip(".dat")
+            try:
+                data_all[sub_key]
+                print(f"Data already read for {path}. Skipping.", flush=True)
+                continue
+            except KeyError:
+                print(f"Loading file {path}", flush=True)
+                data_all[sub_key] = load_psliceout(path)
+    return data_all
 
 
 def load_psliceout(path_psliceout):
@@ -37,3 +86,24 @@ def load_psliceout(path_psliceout):
     raw_idx = raw.index.values
     data = {'raw_coords': raw_coords, 'raw_vels': raw_vels, 'raw_sizes': raw_sizes, 'raw_idx': raw_idx}
     return data
+
+
+def iteratively_rank_neighbors(data_all, paths_psliceout, path_ranked, n_neighbors):
+    """
+    Iteratively ranks neighbors for all psliceout files
+    In:
+        > data_all - (dct) dictionary containing all data
+        > paths_psliceout - (dct) paths to psliceout files
+        > path_ranked - (str) path to ranked file directory
+        > n_neighbors - (int) number of nearest neighbors to find
+    Out:
+        > fout_ranked - (dct) dictionary containing paths to ranked files
+    """
+    print("Ranking particles", flush=True)
+    fout_ranked = {}
+    for key in paths_psliceout:
+        fout_ranked[key] = []
+        for path in paths_psliceout[key]:
+            print(f"Ranking file {path}", flush=True)
+            fout_ranked[key].append(rf.rank_neighbours(data_all, path, path_ranked, n_neighbors))
+    return fout_ranked
--- main_figures_for_pub.py
+++ main_figures_for_pub.py
@@ -3,7 +3,6 @@
 import gen_funcs as gf
 import ranked_funcs as rf
 import clump_funcs as cf
-import json
 import os
 
 # %%
@@ -20,46 +19,20 @@ elif which_figs == "selected":
 
 # %%
 # Load paths
-print("Reading paths", flush=True)
 path_paths = "paths.json"
-with open(path_paths, "r") as f:
-    dict_paths = json.load(f)
-
-# Check that paths exist, if not, create them
-for key in dict_paths:
-    if not os.path.exists(dict_paths[key]):
-        print(f"Creating directory {dict_paths[key]}", flush=True)
-        os.makedirs(dict_paths[key])
+dict_paths = gf.load_paths_all(path_paths)
 
 # %%
 # Load data
-print("Loading data", flush=True)
 path_data = dict_paths["path_data"]
 paths_psliceout = gf.load_paths_psliceout(path_data, figs)
-data_all = {}
-for key in paths_psliceout:
-    for path in paths_psliceout[key]:
-        print(f"Checking if data has already been read into dict", flush=True)
-        sub_key = path.split("/")[-1].strip(".dat")
-        try:
-            data_all[sub_key]
-            print(f"Data already read for {path}. Skipping.", flush=True)
-            continue
-        except KeyError:
-            print(f"Loading file {path}", flush=True)
-            data_all[sub_key] = gf.load_psliceout(path)
+data_all = gf.iteratively_load_data(paths_psliceout)
 
 # %%
 # Rank particles
-print("Ranking particles", flush=True)
 path_ranked = dict_paths["path_ranked"]
 n_neighbors = 1000
-fout_ranked = {}
-for key in paths_psliceout:
-    fout_ranked[key] = []
-    for path in paths_psliceout[key]:
-        print(f"Ranking file {path}", flush=True)
-        fout_ranked[key].append(rf.rank_neighbours(data_all, path, path_ranked, n_neighbors))
+fout_ranked = gf.iteratively_rank_neighbors(data_all, paths_psliceout, path_ranked, n_neighbors)
 
 # # %%
 # # Use ranked data to get COM coordinates
