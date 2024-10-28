f47f9010ee5c43638347ddd7761c9d65333295e4
Adam Koval <adam_koval@hotmail.com>
2024-10-28 17:13:39 +0000

    Functional implementation of worker + automation functions into main ipynb for testing. Add config.json for all declarations. Various bug fixes and re-namings of functions. gen_funcs sees some consolidation of redundant functions, as well as a re-organisation.

--- gen_funcs.py
+++ gen_funcs.py
@@ -4,38 +4,48 @@ import glob
 import inspect
 import json
 import os
+import re
 
 import ranked_funcs as rf
+import clump_funcs as cf
+import plot_funcs as pf
 
 m_swarm = 1.44e-5  # particle mass [code units]
 
-def load_paths_all(path_paths):
+# READ FUNCTIONS
+def read_config(path_config):
     """
-    Loads all path data from .json file
+    Reads in physics.json file
     In:
-        > path_paths - (str) path to paths.json file
+        > path_physics - (str) path to physics.json file
     Out:
-        > dict_paths - (dct) dictionary containing all paths
+        > dict_physics - (dct) dictionary containing physics parameters
     """
     # Report function name
     print(report_function_name(), flush=True)
 
-    # Load paths
-    print("\tReading paths", flush=True)
-    with open(path_paths, "r") as f:
-        dict_paths = json.load(f)
-    
-    # Check that paths exist, if not, create them
-    for key in dict_paths:
-        if not os.path.exists(dict_paths[key]):
-            print(f"\t\tCreating directory {dict_paths[key]}", flush=True)
-            os.makedirs(dict_paths[key])
-    return dict_paths
+    # Load config
+    print("\tReading config", flush=True)
+    with open(path_config, "r") as f:
+        dict_config = json.load(f)
+    return dict_config
 
 
-def load_paths_psliceout(path_data, selected_figs):
+def read_figure_settings(fig_details_csv):
     """
-    Loads paths to psliceout files
+    Reads in figure settings from a .csv file
+    In:
+        > fig_details_csv - (str) path to figure details .csv file
+    Out:
+        > fig_details - (df) dataframe containing figure details
+    """
+    details = pd.read_csv(fig_details_csv, sep=',', header=0)
+    return details
+
+
+def read_paths_psliceout(path_data, selected_figs):
+    """
+    Reads paths to psliceout files
     In:
         > path_data - (str) path to data folder
         > selected_figs - (lst) list of selected figures
@@ -48,6 +58,28 @@ def load_paths_psliceout(path_data, selected_figs):
     return paths_psliceout
 
 
+def load_psliceout(path_psliceout):
+    """
+    Reads in PENCIL slice data file and returns xyz coordinates of all particles in slice
+    In:
+        > path_psliceout - (str) path to psliceout file
+    Out:
+        > data - (dct)
+            raw_coords: xyz coordinates of all particles in slice
+            raw_vels: xyz velocities of all particles in slice
+            raw_sizes: sizes of all particles in slice
+            raw_idx: indices of all particles in slice
+    """
+    raw = pd.read_csv(path_psliceout, sep='\s+', index_col=0, names=['x', 'y', 'z', 'vx', 'vy', 'vz', 'size'], header=None).sort_index()
+    raw_coords = raw[['x', 'y', 'z']].values
+    raw_vels = raw[['vx', 'vy', 'vz']].values
+    raw_sizes = raw['size'].values
+    raw_idx = raw.index.values
+    data = {'raw_coords': raw_coords, 'raw_vels': raw_vels, 'raw_sizes': raw_sizes, 'raw_idx': raw_idx}
+    return data
+
+
+# ITERATIVE FUNCTIONS
 def iteratively_load_data(paths_psliceout):
     """
     Iteratively loads data from psliceout files which are given by paths_psliceout.
@@ -75,27 +107,6 @@ def iteratively_load_data(paths_psliceout):
     return data_all
 
 
-def load_psliceout(path_psliceout):
-    """
-    Reads in PENCIL slice data file and returns xyz coordinates of all particles in slice
-    In:
-        > path_psliceout - (str) path to psliceout file
-    Out:
-        > data - (dct)
-            raw_coords: xyz coordinates of all particles in slice
-            raw_vels: xyz velocities of all particles in slice
-            raw_sizes: sizes of all particles in slice
-            raw_idx: indices of all particles in slice
-    """
-    raw = pd.read_csv(path_psliceout, sep='\s+', index_col=0, names=['x', 'y', 'z', 'vx', 'vy', 'vz', 'size'], header=None).sort_index()
-    raw_coords = raw[['x', 'y', 'z']].values
-    raw_vels = raw[['vx', 'vy', 'vz']].values
-    raw_sizes = raw['size'].values
-    raw_idx = raw.index.values
-    data = {'raw_coords': raw_coords, 'raw_vels': raw_vels, 'raw_sizes': raw_sizes, 'raw_idx': raw_idx}
-    return data
-
-
 def iteratively_rank_neighbors(data_all, paths_psliceout, path_ranked, n_neighbors):
     """
     Iteratively ranks neighbors for all psliceout files
@@ -121,6 +132,101 @@ def iteratively_rank_neighbors(data_all, paths_psliceout, path_ranked, n_neighbo
     return fout_ranked
 
 
+def iteratively_delimit_clumps(config, fout_ranked, data_all):
+    """
+    Function to iteratively delimit clumps in all selected files using the available routines
+    In:
+        > config - (dct) dictionary containing config parameters
+        > fout_ranked - (dct) dictionary containing paths to ranked files (see iteratively_rank_neighbors)
+        > data_all - (dct) dictionary containing all data (see iteratively_load_data)
+    Out:
+        > coords_com_rankedf - (dct) dictionary containing COM coordinates, indices of members, and Hill radii of clumps for all ranked files
+    """
+    # Report function name
+    print(report_function_name(), flush=True)
+
+    # Use ranked data to get COM coordinates
+    print("\tUsing ranked data to get COM coordinates", flush=True)
+
+    # Calculate Roche density as threshold
+    G = config["const_G"]
+    Omega_K = config["const_Omega_K"]
+    H_g = config["const_H_g"]
+    threshold_rho = cf.calculate_density_Roche(G, Omega_K, H_g)
+
+    # Get COM coordinates for all ranked files
+    # Setting up shell parameters
+    dr = config["const_dr"]
+    n_shells = config["const_N_shells"]
+    threshold_radius = config["const_thresh_rad"]
+
+    # Do for all ranked files
+    data_rankedf = {} # used just to check that duplicate analyses don't occur (due to sharing of ranked files between the figures)
+    coords_com_rankedf = {}
+    for folder in fout_ranked:
+        for fin_ranked in fout_ranked[folder]:
+            _fin_ranked = fin_ranked.split("/")[-1].strip(".dat")
+            try:
+                data_rankedf[_fin_ranked]
+                print(f"\t\tFile {_fin_ranked} already processed, skipping.", flush=True)
+            except KeyError:
+                print(f"\t\tProcessing file {_fin_ranked}", flush=True)
+
+                # Consolidate raw data with ranked data
+                data_key = re.search("ranked_[0-9]+neigh_(.+).dat", fin_ranked).group(1)
+                raw_coords, raw_idx = data_all[data_key]['raw_coords'], data_all[data_key]['raw_idx']
+
+                # Get ranked data
+                _data_ranked_red, _data_ranked = rf.read_ranked(fin_ranked, threshold_rho)
+
+                # Get COM coordinates
+                _coords_com_ranked, idxs_clump, R_Hs_final = rf.get_com_coords(_data_ranked_red, dr, n_shells, raw_coords, raw_idx, threshold_radius, var_shell=False)
+
+                # Remove the final "clump" since it would have been empty
+                _coords_com_ranked = {key: _coords_com_ranked[key][:-1] for key in _coords_com_ranked}
+
+                # Save data
+                data_rankedf[_fin_ranked] = _data_ranked_red
+                coords_com_rankedf[_fin_ranked] = {"coords_com": _coords_com_ranked, "idxs_clump": idxs_clump, "R_Hs": R_Hs_final}
+    return coords_com_rankedf
+
+
+def iteratively_plot_figures(selected_figs, paths_psliceout, n_neighbors, data_all, coords_com_rankedf):
+    """
+    Iteratively plots figures
+    """
+    # Report function name
+    print(report_function_name(), flush=True)
+    
+    # Plot figures
+    print("\tPlotting figures", flush=True)
+    for fig in selected_figs:
+        for fin in paths_psliceout[fig]:
+            _fin = fin.split("/")[-1].strip(".dat")
+            _rfin = f"ranked_{n_neighbors}neigh_{_fin}"
+            plot_inst = pf.PlotDustSurfdensEff(_fin, data_all[_fin], coords_com_rankedf[_rfin]['coords_com'], coords_com_rankedf[_rfin]['R_Hs'])
+            plot_inst.plot_surfdens(save=True, show=False)
+
+
+# HELPER FUNCS
+def check_paths(config):
+    """
+    Checks paths specified in config exist & creates them if not
+    In:
+        > config - (dct) dictionary containing config parameters
+    """
+    # Report function name
+    print(report_function_name(), flush=True)
+    
+    # Check that paths exist, if not, create them
+    for key in config:
+        if key.startswith("path_"):
+            if not os.path.exists(config[key]):
+                print(f"\t\tCreating directory {config[key]}", flush=True)
+                os.makedirs(config[key])
+    return None
+
+
 def report_function_name():
     """
     Reports the name of the function that called this function
@@ -133,18 +239,7 @@ def report_function_name():
     return string
 
 
-def read_figure_settings(fig_details_csv):
-    """
-    Reads in figure settings from a .csv file
-    In:
-        > fig_details_csv - (str) path to figure details .csv file
-    Out:
-        > fig_details - (df) dataframe containing figure details
-    """
-    details = pd.read_csv(fig_details_csv, sep=',', header=0)
-    return details
-
-
+# UNITS CLASS
 class units:
     def __init__(self, dust_gas_ratio, npart, code_box_size):
         """
