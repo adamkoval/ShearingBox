2d1a7713eb2ac821671af18867db14975e64912c
Adam Koval <adam_koval@hotmail.com>
2024-10-28 12:01:29 +0000

    Surfdens plot bug fixing

--- plot_funcs.py
+++ plot_funcs.py
@@ -15,11 +15,16 @@ class PlotDustSurfdensEff:
         > COMs - (dct) dictionary containing the COMs
         > R_Hs - (dct) dictionary containing the Hill radii of COMs
     """
-    def __init__(self, data, COMs, R_Hs, res=500):
+    def __init__(self, fin, data, COMs, R_Hs, res=500):
+        # Report function name
+        print(gf.report_function_name(), flush=True)
+        print(f"\tPlotting dust surface density for {fin}", flush=True)
+
+        # Assign variables
         self.data = data
         self.COMs = COMs
         self.R_Hs = R_Hs
-        # self.l = file.split('.')[-2][-1]
+        self.fin = fin
         self.res = res
         self.surfdens = np.zeros((self.res, self.res))
         self.surfdens_norm = np.zeros((self.res, self.res))
@@ -50,24 +55,30 @@ class PlotDustSurfdensEff:
                     self.surfdens_norm[i,j] = 0.0001
                 image1D.append(self.surfdens_norm[i,j])
         self.image2D = np.array(image1D).reshape(self.res, self.res)
+        self.log_image2D = np.log10(self.image2D)
 
     def add_R_Hs(self):
         COMs_x = np.array(self.COMs['x'])
         COMs_y = np.array(self.COMs['y'])
         for i in range(len(COMs_x)):
             circle_H = plt.Circle((COMs_y[i], -COMs_x[i]), self.R_Hs[i], color='r', fill=False)
-            plt.gca().add_artist(circle_H)
+            self.ax.add_patch(circle_H)
 
-    def plot_surfdens(self):
+    def plot_surfdens(self, save=False, show=True):
+        self.fig, self.ax = plt.subplots()
         self.adjust_units(self.data)
         self.update_surfdens(self.data)
         self.populate_image()
         self.add_R_Hs()
-        plt.imshow(np.log10(self.image2D),
+        surfdens_plot = self.ax.imshow(self.log_image2D,
                    extent=[-60.0,60.0,-60.0,60.0],
                    vmin=-1.0,vmax=2.0)
-        cbar = plt.colorbar(orientation='vertical')
-        cbar.set_label('log $(\Sigma_d/<\Sigma_d>)$',rotation=90)
-        plt.xlabel('y')
-        plt.ylabel('x')
-        plt.show()
+        cbar = plt.colorbar(surfdens_plot, orientation='vertical', ax=self.ax)
+        cbar.set_label('log $(\Sigma_d/<\Sigma_d>)$', rotation=90)
+        self.ax.set_xlabel('y')
+        self.ax.set_ylabel('x')
+        self.ax.set_title(self.fin)
+        if save:
+            plt.savefig(f"output/surfdensplots/surfdens_{self.fin}.pdf", format='pdf')
+        if show:
+            plt.show()
