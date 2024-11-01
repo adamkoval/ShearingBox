41fc824e26a6331311ccfb1d22537de41e3f7661
Adam Koval <adam_koval@hotmail.com>
2024-11-01 09:50:09 +0000

    Adapt to the structure created in the dev ipynb

--- main_figures_for_pub.py
+++ main_figures_for_pub.py
@@ -3,34 +3,41 @@
 import gen_funcs as gf
 import ranked_funcs as rf
 import clump_funcs as cf
+import plot_funcs as pf
+
 import os
+import numpy as np
+import re
+from scipy.linalg import norm
 
 # %%
-# Select desired figures
+# Setup
 print("Running setup operations", flush=True)
-selected_figs = ["old"]
-# selected_figs = ["Fig2_Fig4", "Fig6", "Fig7", "Fig8"]
+config = gf.read_config("config.json")
+gf.check_paths(config)
 
 # %%
-# Load paths
-path_paths = "paths.json"
-dict_paths = gf.load_paths_all(path_paths)
+# Assign paths to variables
+path_data = config["path_data"]
+selected_figs = config["arr_figs"] # Folders to plot figures from. Edit config.json as e.g., ["Fig2_Fig4", "Fig6", "Fig7", "Fig8"] or ["old"]
+paths_psliceout = gf.read_paths_psliceout(path_data, selected_figs)
+path_out = config["path_out"]
+path_ranked = config["path_ranked"]
 
 # %%
 # Load data
-path_data = dict_paths["path_data"]
-paths_psliceout = gf.load_paths_psliceout(path_data, selected_figs)
 data_all = gf.iteratively_load_data(paths_psliceout)
 
 # %%
 # Rank particles
-path_ranked = dict_paths["path_ranked"]
-n_neighbors = 100
+path_ranked = config["path_ranked"]
+n_neighbors = config["const_N_neigh"]
 fout_ranked = gf.iteratively_rank_neighbors(data_all, paths_psliceout, path_ranked, n_neighbors)
 
 # %%
-# Use ranked data to get COM coordinates
-print("Using ranked data to get COM coordinates", flush=True)
-for key in fout_ranked:
-    for fin_ranked in fout_ranked[key]:
-        data_ranked, coords_com_ranked = rf.get_com_coords(fin_ranked)
+# Find clumps
+coords_com_rankedf = gf.iteratively_delimit_clumps(config, fout_ranked, data_all)
+
+# %%
+# Plotting
+gf.iteratively_plot_figures(selected_figs, paths_psliceout, n_neighbors, data_all, coords_com_rankedf)
