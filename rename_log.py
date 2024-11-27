9e018e68e3b71962677c5fb69f0856cb3355253e
adamkoval <adam_koval@hotmail.com>
2024-11-27 12:09:59 +0000

    add file to rename each log with timestamp for differentiation

---
+++ rename_log.py
@@ -0,0 +1,10 @@
+#!/home/akoval/miniconda3/envs/python3.11_conda_env/bin/python
+import os
+import sys
+from datetime import datetime
+
+file = sys.argv[1]
+
+os.system(f"mv {file} {file}_{datetime.isoformat(datetime.now())[:-7]}")
+
+sys.exit()
